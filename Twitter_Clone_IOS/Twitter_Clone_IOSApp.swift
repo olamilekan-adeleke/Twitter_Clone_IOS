86f182873a4feb070e346f761800dbf6179fd450
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2023-04-16 13:47:59 +0100

    Initial Commit

---
+++ Twitter_Clone_IOS/Twitter_Clone_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_Clone_IOSApp.swift
+//  Twitter_Clone_IOS
+//
+//  Created by Enigma Kod on 16/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_Clone_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
